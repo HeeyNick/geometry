e9bd836b4470df2895d3255b75b54ae1338b95a7
Патрушев Никита Владимирович <is942s12@awp07.csc.local>
2020-02-11 16:52:41 +0700

    Add function square

--- geometry.c
+++ geometry.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+double square (int a, int b)
+{
+	double sqr = (3,14 * (b*b));
+	return sqr;
+}
+
+
 int main()
 {
-	printf("Hello world");
+	double sqr = 0;
+	sqr = square(0,13);
+	printf("%f", sqr);
 	return 0;
 }

20eb06b15e9b7d1fa9670078c6ea83cfa31b9fb8
HeeyNick <nikitos15032002@gmail.com>
2020-05-28 15:06:06 +0700

    Add test of scan_string

--- test/function_test.c
+++ test/function_test.c
@@ -86,3 +86,82 @@ CTEST(parse_coord, incorrect_y)
     size_t result = parse_coord(&i, &circle[0].x, line);
     ASSERT_EQUAL(expect, result);
 }
+CTEST(scan_string, correct_string)
+{
+    char* line = "circle (1 2, 3)\n";
+    size_t expect = 1;
+    size_t result = scan_string(line, 0, circle);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(scan_string, correct_string_2)
+{
+    char* line = "circle (      1       2     ,       3)\n";
+    size_t expect = 1;
+    size_t result = scan_string(line, 0, circle);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(scan_string, correct_string_3)
+{
+    char* line = "CiRCLE(      1       2     ,       3)\n";
+    size_t expect = 1;
+    size_t result = scan_string(line, 0, circle);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(scan_string, correct_string_4)
+{
+    char* line = "circle(1 2, 0.1)\n";
+    size_t expect = 1;
+    size_t result = scan_string(line, 0, circle);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(scan_string, invalid_string)
+{
+    char* line = "circle (1, 3)\n";
+    size_t expect = 0;
+    size_t result = scan_string(line, 0, circle);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(scan_string, invalid_string_2)
+{
+    char* line = "circle (1 n, 3)\n";
+    size_t expect = 0;
+    size_t result = scan_string(line, 0, circle);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(scan_string, invalid_string_3)
+{
+    char* line = "circle (1, d)\n";
+    size_t expect = 0;
+    size_t result = scan_string(line, 0, circle);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(scan_string, invalid_string_4)
+{
+    char* line = "circle ()\n";
+    size_t expect = 0;
+    size_t result = scan_string(line, 0, circle);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(scan_string, invalid_string_5)
+{
+    char* line = "circle 1 2, 3\n";
+    size_t expect = 0;
+    size_t result = scan_string(line, 0, circle);
+    ASSERT_EQUAL(expect, result);
+}
+
+CTEST(scan_string, invalid_string_6)
+{
+    char* line = "1 2, 3)\n";
+    size_t expect = 0;
+    size_t result = scan_string(line, 0, circle);
+    ASSERT_EQUAL(expect, result);
+}
